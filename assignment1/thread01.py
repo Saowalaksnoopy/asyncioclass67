583f6a841fe8dfff14ec94700fb0ee5f341b8ce0
Saowalaksnoopy <145532401+Saowalaksnoopy@users.noreply.github.com>
2024-06-19 15:03:03 +0700

    submit

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -1,1 +1,19 @@
 # running a function in another thread
+from time import sleep, ctime
+from threading import Thread
+
+#a custom function that blocks for a moment
+def task():
+    #block for a moment
+    sleep(1)
+    #display a message
+    print(f'{ctime()} This is from another thread')
+
+#Create a thread
+thread = Thread(target=task)
+#Run the thread
+thread.start()
+#Wait for the thread to finish 
+print(f'{ctime()} Waiting for the thread...')
+thread.join() 
+
