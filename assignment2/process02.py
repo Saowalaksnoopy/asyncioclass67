fe5b5c67a65b114267a29d3a3fec02ddde0e7f55
Saowalaksnoopy <145532401+Saowalaksnoopy@users.noreply.github.com>
2024-06-26 12:05:12 +0700

    assignment2

--- assignment2/process02.py
+++ assignment2/process02.py
@@ -1,2 +1,54 @@
-# Multiprocessing 2 kitkens, 2 cooker, 2 dishes
+# Multiprocessing 2 kitchens, 2 cookers, 2 dishes
 # share resources
+import multiprocessing
+import os
+from time import sleep, ctime, time
+
+# Basket of sharing
+class Basket:
+    def __init__(self):
+        self.eggs = 50
+        self.lock = multiprocessing.Lock()
+    
+    def use_eggs(self, index):
+        with self.lock:
+            print(f'{ctime()} Kitchen-{index} : Chef-{index} has lock with eggs remaining {self.eggs}')
+            self.eggs -= 1
+            print(f'{ctime()} Kitchen-{index} : Chef-{index} has released lock with eggs remaining {self.eggs}')
+
+# Chef cooking
+def cooking(index, basket):
+    # Chef use eggs for cooking
+    basket.use_eggs(index)
+    sleep(2)
+
+# Kitchen cooking
+def kitchen(index, shared_basket):
+    print(f'{ctime()} Kitchen-{index} : Begin cooking...PID {os.getpid()}')
+    cooking_time = time()
+    cooking(index, shared_basket)
+    duration = time() - cooking_time
+    print(f'{ctime()} Kitchen-{index} : Cooking done in {duration:0.2f} seconds!')
+
+if __name__ == "__main__":
+    # Begin of main thread
+    print(f'{ctime()} Main : Start Cooking...PID {os.getpid()}')
+    start_time = time()
+
+    basket = Basket()
+
+    # Multi processes
+    kitchens = list()
+    for index in range(2):
+        p = multiprocessing.Process(target=kitchen, args=(index, basket))
+        kitchens.append(p)
+        # starting processes
+        p.start()
+
+    for index, p in enumerate(kitchens):
+        # wait until processes are finished
+        p.join()
+
+    print(f'{ctime()} Main : Basket eggs remaining {basket.eggs}')
+    duration = time() - start_time
+    print(f'{ctime()} Main : Finished Cooking duration in {duration:0.2f} seconds')
--- assignment3/asyncio02.py
+++ assignment3/asyncio02.py
@@ -1,2 +1,17 @@
 # example of running a coroutine
+import asyncio
+
+# Define a coroutine
+async def custom_coro():
+    # Await another coroutine
+    await asyncio.sleep(1)
+    print("Custom coroutine completed")
+
+# Main coroutine
+async def main():
+    # Execute my custom coroutine
+    await custom_coro()
+
+# Start the coroutine program
+asyncio.run(main())
 
